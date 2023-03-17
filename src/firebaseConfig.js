6f9bc13753563c021ef14db2f807b84f5ac5bedb
vishnu0921 <ba11@iitbbs.ac.in>
2023-03-17 17:53:32 +0530

    firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmdevyo-ZrbxtrZWUZAT5GM0-vZU_aZhk",
+  authDomain: "drone-delivery-af1d5.firebaseapp.com",
+  projectId: "drone-delivery-af1d5",
+  storageBucket: "drone-delivery-af1d5.appspot.com",
+  messagingSenderId: "515044385956",
+  appId: "1:515044385956:web:ad3916b661b90c94feb231"
+};
+
+const app = initializeApp(firebaseConfig);
